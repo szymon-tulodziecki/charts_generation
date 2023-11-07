66b9ce1f984dc572f6b67757cff363b9358fe392
szymon-tulodziecki <tulodziecki.kontakt@gmail.com>
2023-11-07 14:47:49 +0100

    charts_generation

---
+++ scatterplot/main.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('dane.csv')
+sns.scatterplot(data=data, x="Hz", y="mA")
+plt.xlabel("Hz")
+plt.ylabel("mA")
+plt.title("Wykres Zależności Częstotliwości od Natężenia Prądu")
+plt.show()
